095e3fdbcb9efbfef594fb799d23f6304c033249
AlbertSawZ <albertsawz@gmail.com>
2021-12-28 14:53:55 -0600

    Initial state of the project

---
+++ app/src/main/java/com/albertsawz/dancingrobot/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.albertsawz.dancingrobot
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
