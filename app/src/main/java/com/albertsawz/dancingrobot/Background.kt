43ef0a9148debd206da065fe93119e934fd4fa6d
AlbertSawZ <albertsawz@gmail.com>
2021-12-28 15:04:54 -0600

    Create Background class and set it as content view

---
+++ app/src/main/java/com/albertsawz/dancingrobot/Background.kt
@@ -0,0 +1,12 @@
+package com.albertsawz.dancingrobot
+
+import android.content.Context
+import android.graphics.Canvas
+import android.view.View
+
+class Background(context: Context) : View(context) {
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+}
--- app/src/main/java/com/albertsawz/dancingrobot/MainActivity.kt
+++ app/src/main/java/com/albertsawz/dancingrobot/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val background = Background(this)
+        setContentView(background)
     }
 }
